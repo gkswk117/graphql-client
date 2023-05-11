09a674168c4ed2f3b92b496b14a179d061d7bae0
gkswk117 <gkswk117@gmail.com>
2023-05-11 22:43:52 +0900

    주석 추가.

--- src/client.js
+++ src/client.js
@@ -2,6 +2,8 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
     uri:"http://localhost:4000/",
+    // uri specifies the URL of our GraphQL server.
+    // 노마드코더-graphql-server와 연결.
     cache: new InMemoryCache(),
 })
 
